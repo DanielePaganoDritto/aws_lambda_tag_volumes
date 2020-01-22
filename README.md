a1acb6b790c611b70debf869598178612690c3fb
Daniele Pagano Dritto <daniele.pagano@Toyota-FS.com>
2020-01-22 15:05:38 +0100

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # aws_lambda_tag_volumes
-Lambda Function to tag all volumes inside an AWS account with respective instance name
+
+Python Lambda Function than scans all the existing EBS volumes inside an AWS account tagging those than don't have a Name Tag with the name of the instance their attached to.
+
+
+SNS Notifications
+After every function run the template send an email to an SNS Topic containing a confirmation message for the execution.
+
+For this to work is necessary to pass the Topic Arn and the AWS Account description to which send the notification by creating a test event before running the Lambda function. The test event should have the following structure:
+
+ 
+
+{ "TopicArn": "arn:aws:sns::AccountNumber:TopicName", "AccountDescription": "Account_Name" }
+
+ 
+
+IAM Role and Policy
+To run the Lambda function you need to create/use an IAM Role with the following policies to interact with all the resources needed:
+
+Role Name: AWSLambdaVolumesTaggingRole
+Role Policy:
+Policy 1 Name: Ec2-VolumestaggingPolicy
+    -  "ec2:Describe*"
+    -  "ec2:CreateTags"
+
+Policy 2 Name: SNS-VolumesTaggingPolicy
+    - "sns:Publish*"
+
+** N.B The example ARN above refers to the Topic ARN to use for sending the SNS notification. Change it accordingly when creating the Lambda function on any different account. **
---
+++ aws_lambda_tag_volumes.py
@@ -0,0 +1,63 @@
+#-----26/07/2019-----#
+#--- Script for update the volume tags in which is missing, with instance name tag---
+#!/usr/bin/env python
+ 
+import boto3
+ 
+ec2 = boto3.resource('ec2')
+ec2client = boto3.client('ec2')
+
+ 
+#-----Define Lambda function-----#
+def lambda_handler(event, context):
+ 
+#-----Check& filter Instances which  Kloud_managed equal true-----#
+    instances = ec2client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['*']}])
+ 
+#-----Define dictionary to store Tag Key & value------#
+    dict={}
+ 
+#-----Store Key & Value of Instance Tag:“Name” ------#
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+           for tag in instance['Tags']: 
+                if tag['Key'] == 'Name':
+                    print ( instance['InstanceId'],tag['Value'])
+                    dict[instance['InstanceId']]= tag['Value']
+                    
+     #-----Store Key & Value with attached instance ID of all volumes ------#     
+     
+    #List all the existing volumes
+    volumes = ec2.volumes.all()
+    
+    #Scan the list of volumes
+    for volume in volumes:
+        
+        #Scan all the volumes that are attached to an instance
+        for a in volume.attachments:
+                for key, value in dict.items():
+                    if a['InstanceId'] == key:
+                        
+                        #Filter the volumes without any tag
+                        if not volume.tags:
+      
+                            #Add name Tag to volume
+                            volume.create_tags(Tags=[{'Key': 'Name', 'Value': value}])
+                            
+                        #Check all the volumes that have tags
+                        elif volume.tags:
+                            for tag in volume.tags:
+                                #if Name tag exist but has no value
+                                if tag['Key'] == 'Name' and tag['Value'] == '':
+                                    volume.create_tags(Tags=[{'Key': 'Name', 'Value': value}])
+                                #of tag name doesn't exits
+                                if not tag['Key'] == 'Name':
+                                    volume.create_tags(Tags=[{'Key': 'Name', 'Value': value}])
+                                    
+    #Prepare SNS message 
+    SFCAccount=event['AccountDescription'] 
+    message = ("Volume Tagging Lambda executed for account: %s" % SFCAccount) 
+    subject = ("Lambda Function - Volumes tagging function executed for account %s" % SFCAccount) 
+     
+    #Send notification     
+    sns.publish(TopicArn=event['TopicArn'], Message=(message), Subject=(subject))
